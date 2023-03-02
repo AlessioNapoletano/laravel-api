3746768a5a6e6ab86498555fb169d16e454b00a7
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-02 14:04:21 +0100

    first commit

---
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Technology;
+use Illuminate\Http\Request;
+
+class TechnologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $technologies = Technology::all();
+        return view('admin.technologies.index', compact('technologies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $technology = new Technology();
+        return view('admin.technologies.create', compact('technology'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Technology $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Technology $technology)
+    {
+        return view('admin.technologies.show', compact('technology'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('admin.technologies.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.admin')
+
+@section('title', 'Alessio Napoletano')
+
+@section('content')
+
+<section class="welcome-page">
+    <div class="container mb-5 py-5">
+        <div class="row">
+            @foreach ($projects as $project)
+                <div class="col-6 d-flex flex-wrap align-items-stretch">
+                    <div class="card mb-5">
+                    
+                        @if ( $project->isImageAUrl())
+                            <img src="{{ $project->cover_image }}"
+                        @else
+                            <img src="{{ asset('storage/' . $project->cover_image ) }}"
+                        @endif
+                            alt="{{ $project->title }} image" class="img-fluid">
+    
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold mb-3 d-flex justify-content-around">
+                                <span>
+                                    {{ $project->title }}
+                                </span>
+                                
+                                <span class="btn btn-success rounded-3">
+                                    {{ $project->type->type }}
+                                </span>
+                                
+                            </h5>
+    
+                            <!--Tecnologie utilizzate nel progetto-->
+                            <p class="card-text text-center">
+                                <span class="fw-bold">
+                                    Tecnologie utilizzate: 
+                                </span>
+                                
+                                @foreach ($project->technologies as $tecnology)
+                                <span class="text-light fw-bold p-2 me-1 rounded-2" 
+                                style="background-color: {{ $tecnology->bg_color }};">
+                                    {{ $tecnology->name }}
+                                </span>
+                                @endforeach
+                                
+                            </p>
+
+                            <!--Data di creazione del progetto-->
+                            <p class="card-text text-center">
+                                <span class="fw-bold">
+                                    Data Di Creazione: 
+                                </span>
+    
+                                <span>
+                                    {{ $project->post_date}}
+                                </span>
+                            </p>
+    
+                            <!--Descrizione del progetto-->
+                            <p class="card-text">{{ $project->content }}</p>
+                            
+                        </div>
+    
+                        <div class="button text-center mb-2">
+                            <a href="#" class="btn btn-primary">show</a>
+                        </div>
+                    </div>
+                </div>  
+            @endforeach
+        </div>
+        
+        <div class="container">
+            <div class="fw-bold">
+                {{ $projects->links() }}
+            </div>    
+        </div>
+</section>
+
+@endsection
